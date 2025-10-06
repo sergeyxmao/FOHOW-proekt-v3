d66ee46c3b01c3d420e33684de240247efaa4201
sergeyxmao <68888658+sergeyxmao@users.noreply.github.com>
2025-10-06 21:43:51 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,12 @@ import { renderApp } from "./app.js";
 
 function mountApp() {
   hideAuthModal();
+  const boot = document.getElementById("boot-hint");
+  if (boot) boot.remove();
   renderApp();
 }
 
+console.info("[main] стартую…");
 authStore.checkFromStorage();
 
 if (authStore.isAuthed) {
