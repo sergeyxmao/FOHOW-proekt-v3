c4dbc8189029adea54d4038b6bc3e9a245e8c7d3
sergeyxmao <68888658+sergeyxmao@users.noreply.github.com>
2025-10-30 11:38:52 +0500

    Update base path in Vite configuration
    Change base path for deployment to GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/FOHOW-proekt-v3/', // Базовый путь для GitHub Pages
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
