5052bced6727224d0dc66b8227d5e2e6ac57b3b1
sergeyxmao <68888658+sergeyxmao@users.noreply.github.com>
2025-10-30 18:17:48 +0500

    Update BoardComments.vue

--- src/components/Panels/BoardComments.vue
+++ src/components/Panels/BoardComments.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, computed } from 'vue'
 import { storeToRefs } from 'pinia'
-import { useBoardCommentsStore } from '../../stores/boardComments'
+import { useBoardCommentsStore } from './boardComments.js'
 
 const commentsStore = useBoardCommentsStore()
 const { orderedComments, hasComments } = storeToRefs(commentsStore)
