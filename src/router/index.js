69fc96d12ee7a6a3bf89ae978f03564b1e4fd5ae
sergeyxmao <68888658+sergeyxmao@users.noreply.github.com>
2025-11-12 02:36:31 +0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
-<!DOCTYPE html>
-<html lang="ru">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Тест роутера Vue</title>
-  </head>
-  <body>
-    <div id="app"></div>
-  </body>
-</html>
+import { createRouter, createWebHistory } from 'vue-router'
+
+const HomeView = { template: '<h1>HOME PAGE</h1>' }
+const PricingTest = { template: '<h1>ПРИЦИНГ ПУБЛИК ✅</h1>' }
+const TestPage = { template: '<h1>ТЕСТ СТРАНИЦА ✅</h1>' }
+
+const routes = [
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/pricing', name: 'pricing', component: PricingTest },
+  { path: '/test', name: 'test', component: TestPage }
+]
+
+export default createRouter({
+  history: createWebHistory('/'),
+  routes
+})
